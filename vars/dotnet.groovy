272598fed8e0bca8e565af99618f53631175f5e2
Hiep Tran Hoang <hiep.tranhoang@nashtechglobal.com>
2023-08-14 06:20:04 +0700

    Add config

--- vars/dotnet.groovy
+++ vars/dotnet.groovy
@@ -1,16 +1,16 @@
 #!/usr/bin/env groovy
 void call() {
-    String name = "bookstore"
+    String name = "backend-nashtech-hieptran-sd4871"
     String runtime = "BookStore.API.dll"
     String publishProject = "src/BookStore.API/BookStore.API.csproj"
     String baseImage     = "mcr.microsoft.com/dotnet/sdk"
     String baseTag       = "6.0"
-    String demoRegistry = "demotraining.azurecr.io"
+    String demoRegistry = "913820192915.dkr.ecr.ap-southeast-1.amazonaws.com"
     String checkBranches = "$env.BRANCH_NAME"
     String[] deployBranches = ['main', 'jenkins']
     String sonarToken = "sonar-token"
-    String acrCredential = 'acr-demo-token'
-    String k8sCredential = 'akstest'
+    String acrCredential = 'ecr-token'
+    String k8sCredential = 'eks-token'
     String namespace = "demo"
     String rununitTest = "dotnet test --no-build -l:trx -c Release -p:DOTNET_RUNTIME_IDENTIFIER=linux-x64 --collect:'XPlat Code Coverage' --verbosity minimal --results-directory ./results"
 
