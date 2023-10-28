9aea3bbf924c447b9863962cc3211473c7e7bd5f
Tran Hoang Hiep <hiepgia@gmail.com>
2023-10-28 14:18:07 +0700

    Update trivy stage

--- vars/nodeFrontend.groovy
+++ vars/nodeFrontend.groovy
@@ -28,9 +28,7 @@ void call() {
     }
 
     stage ("Trivy scan") {
-        // Install trivy
-        // sh 'curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin v0.18.3'
-        // sh 'curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/html.tpl > html.tpl'
+        sh 'curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/html.tpl > html.tpl'
 
         // Scan all vuln levels
         sh 'mkdir -p reports'
