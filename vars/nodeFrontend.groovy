5c3afdd4f9548cb48f5a8645265f26c7e76f3422
Hiep Tran Hoang <hiep.tranhoang@nashtechglobal.com>
2023-10-28 08:43:47 +0700

    Add helm deploy on Argo & Add trivi stage to jenkins shared library

--- vars/nodeFrontend.groovy
+++ vars/nodeFrontend.groovy
@@ -27,6 +27,28 @@ void call() {
         ${WORKSPACE}/src/frontend")
     }
 
+    stage ("Trivy scan") {
+        // Install trivy
+        // sh 'curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin v0.18.3'
+        // sh 'curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/html.tpl > html.tpl'
+
+        // Scan all vuln levels
+        sh 'mkdir -p reports'
+        sh 'trivy filesystem --ignore-unfixed --vuln-type os,library --format template --template "@html.tpl" -o reports/frontend-scan.html ./src/frontend'
+        publishHTML target : [
+            allowMissing: true,
+            alwaysLinkToLastBuild: true,
+            keepAll: true,
+            reportDir: 'reports',
+            reportFiles: 'frontend-scan.html',
+            reportName: 'Trivy Scan',
+            reportTitles: 'Trivy Scan'
+        ]
+
+        // Scan again and fail on CRITICAL vulns
+        sh 'trivy filesystem --ignore-unfixed --vuln-type os,library --exit-code 1 --severity CRITICAL ./src/frontend'
+    }
+
     stage ("Push Docker Images") {
         docker.withRegistry("https://${registry}", ecrCredential) {
             docker.image("${registry}/${name}:${BUILD_NUMBER}").push()
